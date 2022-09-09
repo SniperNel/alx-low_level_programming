4d8d65e544ac4821ee759d7daf26459bea7845e7
Justin <justinezema@gmail.com>
2022-09-09 15:48:10 -0700

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 'A'; J <= 'Z'; J++)
+	{
+		putchar(j)
+	}
+	putchar('\n');
+	return (0);
+
+}
